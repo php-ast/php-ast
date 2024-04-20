a3bf43ab22fd821f78baea43fadf87151d167026
php-ast <20200120614@nxmu.edu.cn>
2024-04-20 17:37:18 +0800

    update

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="php_ast",
-    version="1.1",
+    version="1.2",
     packages=find_packages(),
     package_data={
         'php_ast': ['*.so'],
